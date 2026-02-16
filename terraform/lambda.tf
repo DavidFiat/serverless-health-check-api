5c9b1e53884753fd93e743e807ee0e973cb8e8c4
DavidFiat <davidfiat24@gmail.com>
2026-02-15 23:18:28 -0500

    add Lambda function  packaging

---
+++ terraform/lambda.tf
@@ -0,0 +1,35 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../lambda/index.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "health_check" {
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = "${var.environment}-health-check-function"
+  role            = aws_iam_role.lambda_execution.arn
+  handler         = "index.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime         = var.lambda_runtime
+  memory_size     = var.lambda_memory
+  timeout         = var.lambda_timeout
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.requests.name
+    }
+  }
+
+  tags = {
+    Name = "${var.environment}-health-check-function"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.environment}-health-check-function"
+  retention_in_days = 7
+
+  tags = {
+    Name = "${var.environment}-health-check-logs"
+  }
+}
